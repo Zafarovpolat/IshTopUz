0a534691d525aceb689de11f422ca54a67318f7e
zafarovpolat <atuin59354081@gmail.com>
2025-08-11 03:37:01 +0700

    suspend created at survey form

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,8 +1,7 @@
-// Import the functions you need from the SDKs you need
+// @/lib/firebase.ts
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 export const firebaseConfig = {
   projectId: "ishtop-landing",
   appId: "1:895516153164:web:e17e28fa7eb259683e2be9",
@@ -13,7 +12,6 @@ export const firebaseConfig = {
   messagingSenderId: "895516153164",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
